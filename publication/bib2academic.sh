1717ccb5ca5a8a9c5c0d96a602cdc44fa9c714dd
baranaydogan <baran.aydogan@gmail.com>
2024-01-09 20:27:01 +0000

    deploy: cnir-group/website@d02478a3cafbfca3080277f9a683c0a125eecdd8

--- publication/bib2academic.sh
+++ publication/bib2academic.sh
@@ -4,11 +4,23 @@ cd ../..
 
 # pip install -U academic
 
-academic import --overwrite --bibtex content/publication/pubs.bib
+academic import --overwrite --bibtex content/publication/pubs.bib content/publication
 
 cd content/publication
-
 
 find . -name "*.md" -type f -exec sed -i 's/Dogu Baran Aydogan/admin/g' {} +
 find . -name "*.md" -type f -exec sed -i 's/Baran Aydogan/admin/g' {} +
 find . -name "*.md" -type f -exec sed -i 's/Joonas Laurinoja/joonas/g' {} +
+
+
+find . -name "*.md" -type f -exec sed -i 's/uncategorized/0/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/paper-conference/1/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/article-journal/2/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/preprint/3/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/report/4/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/book/5/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/book-section/6/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/thesis/7/g' {} +
+find . -name "*.md" -type f -exec sed -i 's/patent/8/g' {} +
+
+find . -name "*.md" -type f -exec sed -i 's/manuscript/3/g' {} +
