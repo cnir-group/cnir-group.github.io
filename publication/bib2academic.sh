fa72d3fb895bc25d2b5aed67cb041f51d13917c9
baranaydogan <baran.aydogan@gmail.com>
2024-09-12 19:46:29 +0000

    deploy: cnir-group/website@4ebe016f5dec0934dd71a91be86c7da9158809dd

--- publication/bib2academic.sh
+++ publication/bib2academic.sh
@@ -13,14 +13,15 @@ find . -name "*.md" -type f -exec sed -i 's/Baran Aydogan/admin/g' {} +
 find . -name "*.md" -type f -exec sed -i 's/Joonas Laurinoja/joonas/g' {} +
 
 
-find . -name "*.md" -type f -exec sed -i 's/uncategorized/0/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/paper-conference/1/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/article-journal/2/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/preprint/3/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/report/4/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/book/5/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/book-section/6/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/thesis/7/g' {} +
-find . -name "*.md" -type f -exec sed -i 's/patent/8/g' {} +
 
-find . -name "*.md" -type f -exec sed -i 's/manuscript/3/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/uncategorized/0/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/paper-conference/1/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/article-journal/2/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/preprint/3/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/report/4/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/book/5/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/book-section/6/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/thesis/7/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/patent/8/g' {} +
+# find . -name "*.md" -type f -exec sed -i 's/manuscript/3/g' {} +
+
